62aef870d3bd51fdc1826efe74368165e39f155d
chinmaynet <ccchinmaysinnn@gmail.com>
2023-12-29 17:44:31 +0530

    Add project files.

---
+++ BlazorGameApp/Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace BlazorGameApp.Shared
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
